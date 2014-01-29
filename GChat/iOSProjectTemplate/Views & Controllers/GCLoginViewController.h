508fc3c29c63f6bc07060c3a253fdf09b168a5f2
Carlin <email.me@carlinyuen.com>
2014-01-29 16:51:54 -0500

    adding login screen

---
+++ GChat/iOSProjectTemplate/Views & Controllers/GCLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  GCLoginViewController.h
+//  GChat
+//
+//  Created by . Carlin on 1/29/14.
+//  Copyright (c) 2014 Carlin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GCLoginViewController : UIViewController
+
+@end
---
+++ GChat/iOSProjectTemplate/Views & Controllers/GCLoginViewController.m
@@ -0,0 +1,38 @@
+//
+//  GCLoginViewController.m
+//  GChat
+//
+//  Created by . Carlin on 1/29/14.
+//  Copyright (c) 2014 Carlin. All rights reserved.
+//
+
+#import "GCLoginViewController.h"
+
+@interface GCLoginViewController ()
+
+@end
+
+@implementation GCLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
